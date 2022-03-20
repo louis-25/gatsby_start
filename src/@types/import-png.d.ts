9c5ccef4e4ab911bba28b078ce94007cf302f7b9
louis-25 <20151577@vision.hoseo.edu>
2022-03-20 21:52:59 +0900

    gatsby 초기세팅

---
+++ src/@types/import-png.d.ts
@@ -0,0 +1,9 @@
+declare module '*.png' {
+  const content: string
+  export default content
+}
+
+declare module '*.PNG' {
+  const content: string
+  export default content
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,84 @@
+import React, { FunctionComponent } from 'react'
+import styled from '@emotion/styled'
+import GlobalStyle from 'components/Common/GlobalStyle'
+import Footer from 'components/Common/Footer'
+import CategoryList from 'components/Main/CategoryList'
+import Introduction from 'components/Main/Introduction'
+import PostList from 'components/Main/PostList'
+import { PostListItemType } from 'types/PostItem.types'
+import { IGatsbyImageData } from 'gatsby-plugin-image'
+import { graphql } from 'gatsby'
+
+type IndexPageProps = {
+  data: {
+    allMarkdownRemark: {
+      edges: PostListItemType[]
+    }
+    file: {
+      childImageSharp: { gatsbyImageData: IGatsbyImageData }
+    }
+  }
+}
+
+const CATEGORY_LIST = {
+  All: 5,
+  Web: 3,
+  Mobile: 2,
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`
+
+const IndexPage: FunctionComponent<IndexPageProps> = function ({
+  data: {
+    allMarkdownRemark: { edges },
+    file: {
+      childImageSharp: { gatsbyImageData },
+    },
+  },
+}) {
+  return (
+    <Container>
+      <GlobalStyle />
+      <Introduction profileImage={gatsbyImageData} />
+      <CategoryList selectedCategory="Web" categoryList={CATEGORY_LIST} />
+      <PostList posts={edges} />
+      <Footer />
+    </Container>
+  )
+}
+
+export default IndexPage
+
+export const getPostList = graphql`
+  query getPostList {
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date, frontmatter___title] }
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            summary
+            date(formatString: "YYYY.MM.DD.")
+            categories
+            thumbnail {
+              childImageSharp {
+                gatsbyImageData(width: 768, height: 400)
+              }
+            }
+          }
+        }
+      }
+    }
+    file(name: { eq: "profile-image" }) {
+      childImageSharp {
+        gatsbyImageData(width: 120, height: 120)
+      }
+    }
+  }
+`
