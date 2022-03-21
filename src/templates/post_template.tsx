4ba29f7617cc9c2a3c8a6f7156d9b0e928abfece
louis-25 <20151577@vision.hoseo.edu>
2022-03-22 00:49:18 +0900

    템플릿 컴포넌트 내에서 Slug에 맞는 데이터 조회하기

--- src/templates/post_template.tsx
+++ src/templates/post_template.tsx
@@ -1,11 +1,55 @@
 import React, { FunctionComponent } from 'react'
+import { PostPageItemType } from 'types/PostItem.types'
+import { graphql } from 'gatsby'
 
-type PostTemplateProps = {}
+type PostTemplateProps = {
+  data: {
+    allMarkdownRemark: {
+      edges: PostPageItemType[]
+    }
+  }
+  location: {
+    href: string
+  }
+}
 
-const PostTemplate: FunctionComponent<PostTemplateProps> = function (props) {
-  console.log(props)
-
-  return <div>Post Template</div>
+const PostTemplate: FunctionComponent<PostTemplateProps> = function ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+  location: { href },
+}) {
+  console.log(edges)
+  console.log(href)
+  return (
+    <>
+      <div>edge </div>
+      <div>location </div>
+    </>
+  )
 }
 
 export default PostTemplate
+
+export const queryMarkdownDataBySlug = graphql`
+  query queryMarkdownDataBySlug($slug: String) {
+    allMarkdownRemark(filter: { fields: { slug: { eq: $slug } } }) {
+      edges {
+        node {
+          html
+          frontmatter {
+            title
+            summary
+            date(formatString: "YYYY.MM.DD.")
+            categories
+            thumbnail {
+              childImageSharp {
+                gatsbyImageData
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
