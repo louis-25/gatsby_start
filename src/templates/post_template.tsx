c73203b2ef258ff3f27270baf4fc4fd2ad9a96fb
louis-25 <20151577@vision.hoseo.edu>
2022-03-23 01:24:40 +0900

    게시글 페이지 상단 부분 구현

--- src/templates/post_template.tsx
+++ src/templates/post_template.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent } from 'react'
 import { PostPageItemType } from 'types/PostItem.types'
 import { graphql } from 'gatsby'
 import Template from 'components/Common/Template'
+import PostHead from 'components/Post/PostHead'
 
 type PostTemplateProps = {
   data: {
@@ -20,9 +21,31 @@ const PostTemplate: FunctionComponent<PostTemplateProps> = function ({
   },
   location: { href },
 }) {
-  console.log(edges)
-  console.log(href)
-  return <Template>Post Template</Template>
+  const {
+    node: {
+      html,
+      frontmatter: {
+        title,
+        summary,
+        date,
+        categories,
+        thumbnail: {
+          childImageSharp: { gatsbyImageData },
+          // publicURL,
+        },
+      },
+    },
+  } = edges[0]
+  return (
+    <Template>
+      <PostHead
+        title={title}
+        date={date}
+        categories={categories}
+        thumbnail={gatsbyImageData}
+      />
+    </Template>
+  )
 }
 
 export default PostTemplate
